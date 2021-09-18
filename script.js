0d0e2b97ce74b240ff2b82337ad50ddca449d90b
JNM87 <J.molina22587@gmail.com>
2021-09-17 22:39:06 -0400

    third commit

---
+++ script.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+  $('.saveBtn').on('click', function () {
+    
+    var value = $(this).siblings('.description').val();
+      var time = $(this).parent().attr('id');
+
+    // localStorage 
+    localStorage.setItem(time, value);
+
+  
+    $('.notification').addClass('show');
+
+    setTimeout(function () {
+      $('.notification').removeClass('show');
+    }, 5000);
+  });
+
+ 
+
+  function hourUpdater() {
+    var currentHour = moment().hours();
+
+    $('.time-block').each(function () {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).deleteClass('past');
+
+        $(this).addClass('present');
+      } else {
+        $(this).deleteClass('past');
+
+          $(this).deleteClass('present');
+
+            $(this).addClass('future');
+        }
+    });
+  }
+
+  hourUpdater();
+
+  // its a interval to check if the current time needs to be updated
+    var interval = setInterval(hourUpdater, 15000);
+
+  // this part loads and saves data from localStorage
+  $('#hour-7 .description').val(localStorage.getItem('hour-7'));
+
+  $('#hour-8 .description').val(localStorage.getItem('hour-8'));
+
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+
+  // the date of today
+  $('#currentDay').text(moment().format('dddd, MMMM Do'));
+  
+});
+
